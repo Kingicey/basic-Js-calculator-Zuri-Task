9fabc6877ae4ea17b0f8f63962e3013f4c1f20d3
Okolo Ifeanyi Christian <iceyokolo@gmail.com>
2022-06-25 16:41:59 +0100

    Updated

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-num1 = prompt("Enter first number: ");
+let num1 = parseFloat(prompt("Enter first number: "));
 
-num2 = prompt("Enter second number: ");
+let num2 = parseFloat(prompt("Enter second number: "));
 
-operator = prompt("Choose operator: +, -, /, *...");
+let operator = prompt("Choose operator: +, -, /, *...");
 
 if (operator === "+") {
-  result = num1 + num2;
+  let result = num1 + num2;
   console.log(`${num1} ${operator} ${num2} equals ${result}`);
 } else if (operator === "-") {
-  result = num1 - num2;
+  let result = num1 - num2;
   console.log(`${num1} ${operator} ${num2} equals ${result}`);
 } else if (operator === "*") {
-  result = num1 * num2;
+  let result = num1 * num2;
   console.log(`${num1} ${operator} ${num2} equals ${result}`);
 } else if (operator === "/") {
-  result = num1 / num2;
+  let result = num1 / num2;
   console.log(`${num1} ${operator} ${num2} equals ${result}`);
 } else {
   console.log("Enter valid operator");
